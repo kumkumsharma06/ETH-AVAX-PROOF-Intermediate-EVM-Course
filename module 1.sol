48914226c3e4feec353f6476e0cd788f3b1f04e0
kumkumsharma06 <135731376+kumkumsharma06@users.noreply.github.com>
2023-07-01 11:55:33 +0000

    module 1.sol

--- module 1.sol
+++ module 1.sol
@@ -3,16 +3,19 @@ pragma solidity ^0.8.17;
 
 contract ErrorHandling {
     uint b=10;
-    function testAssert(uint num) public pure{
+    // assert function
+    function Assert(uint num) public pure{
         assert(num!=0);
     }
-    function testRevert(uint _numerator, uint _denomenator) public pure returns (uint){
+    // revert function
+    function divideRevert(uint _numerator, uint _denomenator) public pure returns (uint){
         if(_numerator<_denomenator){  
             revert("numerator should be greater than denomenator according to condition");        
         }
         return _numerator/_denomenator;
     }
-    function testRequire(uint a) public view returns (uint){
+    //require function
+    function multiplicationRequire(uint a) public view returns (uint){
         require(a>0,"Value of a is zero , as expecting result should not be zero");
         return a*b;
     }
